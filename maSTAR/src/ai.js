468e02aedde6a7b06bfacd44baf69695cd1207e8
Stevin006 <Steving2006@gmail.com>
2025-09-20 15:38:07 -0400

    InitalAI

---
+++ maSTAR/src/ai.js
@@ -0,0 +1,13 @@
+import Vapi from "@vapi-ai/web"
+
+export const vapi = new Vapi(import.meta.env.VITE_VAPI_API_KEY)
+const assistantId = import.meta.env.VITE_ASSISTANT_ID
+
+export const startAssistant = async(firstName, lastName, email, phone) => {
+    const assistantOverrides = {}
+    return await vapi.start(assistantId);
+};
+
+export const stopAssitant = () => {
+  vapi.stop();  
+}
